2234beebc53079086adffc3d7bb24370c89aa4f7
JuanIgnacio2 <juanignacioleivachaves@gmail.com>
2025-12-04 16:33:25 -0300

    Cambio URL de conexion

--- src/services/axiosClient.js
+++ src/services/axiosClient.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const axiosClient = axios.create({
     //baseURL: import.meta.env.API_URL || 'http://localhost:8080/api',
     //baseURL: 'http://localhost:8080/api/',
-    baseURL: 'http://192.168.1.101:8080/api/',
-    //baseURL: 'https://backend-liquidaci-n-25-de-mayo.onrender.com/api/',
+    //baseURL: 'http://192.168.1.101:8080/api/',
+    baseURL: 'https://backend-liquidaci-n-25-de-mayo.onrender.com/api/',
     headers: {'Content-Type' : 'application/json'},
     timeout: 10_000,
 });
